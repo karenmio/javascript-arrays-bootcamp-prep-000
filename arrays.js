cc8fa652131dbe64a9ae8915dd4bde54bacd3561
karen wang <karenkarenmio@gmail.com>
2018-05-07 17:53:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,13 +36,12 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
 
-
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array;
 }
 
-function removeElementFromEndOfArray() {
+function removeElementFromEndOfArray(array) {
   return array.slice(0,array.length-1);
 }
 
